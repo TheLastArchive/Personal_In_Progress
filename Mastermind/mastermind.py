8b7b8958bd66ec59fab4a9c5c678d8caef752be5
jwolf <jwolf@student.wethinkcode.co.za>
2020-08-27 16:15:23 +0200

    Updated Mastermind

--- Mastermind/mastermind.py
+++ Mastermind/mastermind.py
@@ -6,52 +6,70 @@ def code_generator():
     i = 0
 
     while i < 4:
-        code[i] = random.randint(1, 8)
-        i += 1
+        x = random.randint(1, 8)
+        if x not in code:
+            code[i] = x
+            i += 1
+    
+    return "".join(map(str, code))
 
-    return code
 
+def valid_input(user_in):
 
-def input_check(code, user_in):
+    if len(user_in) != 4 or user_in.isnumeric() == False: return False
 
-    temp = list(map(int, user_in))
+    range_check = ['0', '9']
+    for x in user_in:
+        if x in range_check: return False
+        
+    return True
+
+def code_compare(code, user_in):
+
     code = list(code)
-    i = 0
+    temp = code
+    index = 0
     place_check = 0
     unit_check = 0
 
-    while i < 4:
-        if code[i] == temp[i]: 
+    while index < 4:
+        if code[index] == user_in[index]: 
             place_check += 1
-        i += 1
+            temp[index] = '_'
+        index += 1
+
+    for x in user_in:
+        if x in temp:
+            index = temp.index(x)
+            temp[index] = '_'
+            unit_check += 1
 
     return(place_check, unit_check)
+
 
 def run_game():
     
     code = code_generator()
-    print(code)
     guesses = 12
     print("4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.")
     user_in = input("Input 4 digit code: ")
 
-    while list(user_in) != code or guesses != 0:
-        if len(user_in) != 4 and user_in.isnumeric == False:
+    while guesses != 1:
+        if valid_input(user_in) == False:
             print("Please enter exactly 4 digits.")
             user_in = input("Input 4 digit code: ")
         else:
-            place_check, unit_check = input_check(code, user_in)
+            place_check, unit_check = code_compare(code, user_in)
+            print("Number of correct digits in correct place:    " , place_check)
+            print("Number of correct digits not in correct place:" , unit_check)
             if place_check == 4:
                 break
-            print("Number of correct digits in correct place: " , place_check)
-            print("Number of correct digits not in correct place: " , unit_check)
             guesses -= 1
-            print("Turns left: " , guesses)
+            print("Turns left:" , guesses)
             user_in = input("Input 4 digit code: ")
     
-    if guesses != 0: print("Congratulations! Your are a codebreaker!")
-    else: print("The code was: " , code)
-
+    if guesses != 1: print("Congratulations! You are a codebreaker!")
+    print("The code was:", "".join(code))
 
 
 if __name__ == "__main__":
