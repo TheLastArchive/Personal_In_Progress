320b8a1567b7cad5e9812f9a92f682d3fae0455a
TheLastArchive <iarchive@hotmail.com>
2020-11-05 17:51:25 +0200

    Added Loot Price Checker project

---
+++ Loot_Price_Checker/alert.py
@@ -0,0 +1,24 @@
+import smtplib
+
+def send_email():
+
+    USER = 'loot.price.check@gmail.com'
+    APP_PASSWORD = 'alsnzvmaziseeuaz'
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(USER, APP_PASSWORD)
+
+        subject = 'Test'
+        body = 'This is a test'
+
+        msg = f'Subject: {subject}\n\n{body}'
+
+        server.sendmail(USER, 'iarchive@hotmail.com', msg)
+        
+        #server.quit()
+
+
+#send_email('awood@student.wethinkcode.com', "Test", "This is a test")
